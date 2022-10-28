20f945eb2fce344912a167429c92a8ccdc290a48
alexisterrified <alexepicity@gmail.com>
2022-10-28 15:45:32 -0400

    charts added

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-
+import Chart from 'react-apexcharts'
 import {NewIncome, NewExpenses} from './newUser'
 
 export default function Dashboard({income,expenses}){
@@ -10,7 +10,26 @@ export default function Dashboard({income,expenses}){
 			<NewExpenses/>
 		</div>
 	)
+
+	const donutOptions = {
+        labels: ['A', 'B', 'C', 'D', 'E'],
+      }
+	const donutSeries = [44, 55, 41, 17, 15]
 	
+	const areaOptions = {
+					xaxis: {
+					  categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+					},
+					chart:{toolbar:{tools:{download:true,selection:false,zoom:false,zoomin:false,zoomout:false,pan:false,reset:false}},selection:{enabled:false}}
+				  }
+				  
+	const areaSeries = [{
+							name: 'series-1',
+							data: [30, 40, 25, 50, 49, 21, 70, 51]
+						  }, {
+							name: 'series-2',
+							data: [23, 12, 54, 61, 32, 56, 81, 19]
+						  }]
 	
 	return (
 		<div className="dashboard">
@@ -18,8 +37,12 @@ export default function Dashboard({income,expenses}){
 			</div>
 			<div className="charts">
 				<div className="pie-chart">
+					<div className="pie">
+						<Chart options={donutOptions} series={donutSeries} type="donut" />
+					</div>
 				</div>
 				<div className="graph">
+				<Chart options={areaOptions} series={areaSeries} type="area" />
 				</div>
 			</div>
 			<div className="lists">
